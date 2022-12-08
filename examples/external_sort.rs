2d5e682a17f710254184ff336a37bed1f8140b2b
akmitrich <ak.mitrich@mail.ru>
2022-12-08 16:18:07 +0300

    External Sort naive

--- examples/external_sort.rs
+++ examples/external_sort.rs
@@ -1,16 +1,53 @@
 use dotenv::{dotenv, var};
-use sort_playground::external::create_file_random_numbers;
-use std::{io, path::Path};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
 
 fn main() -> io::Result<()> {
     dotenv().unwrap();
     let parameter_basepath = var("BASEPATH").unwrap_or_else(|_| String::from("."));
-    let parameter_method = var("METHOD").unwrap_or_else(|_| String::from("naive"));
+    let _parameter_method = var("METHOD").unwrap_or_else(|_| String::from("naive"));
     let parameter_input = var("INPUT").unwrap_or_else(|_| String::from("input.txt"));
     let parameter_output = var("OUTPUT").unwrap_or_else(|_| String::from("output.txt"));
     let parameter_chunksize = var("CHUNKSIZE").unwrap_or_else(|_| String::from("N/A"));
     let base_path = Path::new(&parameter_basepath);
     let chunk_size: usize = parameter_chunksize.parse().unwrap_or(1000);
-    println!("{parameter_method:?} {base_path:?} {chunk_size}");
-    Ok(())
+    match sort_playground::external::naive(
+        base_path,
+        &parameter_input,
+        &parameter_output,
+        chunk_size,
+    ) {
+        Ok(_) => {
+            println!(
+                "Sort complete. Sorted {}%",
+                sorted_percent(base_path.join(parameter_output))?
+            );
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
+}
+
+fn sorted_percent(path: impl AsRef<Path>) -> io::Result<usize> {
+    let sorted = File::open(path)?;
+    let mut data = BufReader::new(sorted)
+        .lines()
+        .filter_map(|x| x.ok())
+        .filter_map(|x| x.parse::<i64>().ok());
+    let start = data.next().unwrap();
+    let (sum, success, _) = data.fold((0, 0, start), |(sum, success, prev), current| {
+        (
+            sum + 1,
+            if prev <= current {
+                success + 1
+            } else {
+                success
+            },
+            current,
+        )
+    });
+    Ok(if sum > 0 { success * 100 / sum } else { 100 })
 }
--- src/external.rs
+++ src/external.rs
@@ -1,10 +1,14 @@
 use std::{
-    fs::File,
-    io::{self, Write},
+    cmp::Ordering,
+    fs::{self, File},
+    io::{self, BufRead, Write},
+    iter::Peekable,
     path::Path,
 };
 
 use rand::Rng;
+
+use crate::{merge::merge_sort, sort_playground::SortPlayground};
 
 pub fn create_file_random_numbers(path: impl AsRef<Path>, n: usize, t: i64) -> io::Result<()> {
     let mut file = File::create(path)?;
@@ -14,4 +18,129 @@ pub fn create_file_random_numbers(path: impl AsRef<Path>, n: usize, t: i64) -> i
     }
     write!(file, "{}", rng.gen_range(1..=t))?;
     Ok(())
+}
+
+pub fn naive(
+    path: impl AsRef<Path>,
+    input: &str,
+    output: &str,
+    chunk_size: usize,
+) -> io::Result<()> {
+    let input = File::open(path.as_ref().join(input))?;
+    create_sorted_chunks(&path, &input, chunk_size)?;
+    merge_sorted_chunks(&path)?;
+    fs::rename(path.as_ref().join("0.chunk"), path.as_ref().join(output))?;
+    Ok(())
+}
+
+fn create_sorted_chunks(path: impl AsRef<Path>, input: &File, chunk_size: usize) -> io::Result<()> {
+    let mut chunk = SortPlayground::default();
+    let mut chunk_number = 0_usize;
+    for n in num_file_iter(input) {
+        chunk.data.push(n);
+        if chunk.data.len() == chunk_size {
+            chunk = merge_sort(chunk);
+            let chunk_path = path.as_ref().join(format!("{chunk_number}.chunk"));
+            create_file_from_iter(chunk_path, chunk.data.iter().copied())?;
+            chunk_number += 1;
+            chunk.data.clear();
+        }
+    }
+    let chunk_path = path.as_ref().join(format!("{chunk_number}.chunk"));
+    create_file_from_iter(chunk_path, chunk.into_iter())?;
+    Ok(())
+}
+
+fn merge_sorted_chunks(path: impl AsRef<Path>) -> io::Result<()> {
+    let mut chunk_number = 0_usize;
+    let mut merged_number = 0_usize;
+    while let Ok(left_file) = File::open(path.as_ref().join(format!("{}.chunk", chunk_number))) {
+        chunk_number += 1;
+        if let Ok(right_file) = File::open(path.as_ref().join(format!("{}.chunk", chunk_number))) {
+            chunk_number += 1;
+            let merged = Ascending::new(num_file_iter(&left_file), num_file_iter(&right_file));
+            create_file_from_iter(
+                path.as_ref().join(format!("m{merged_number}.chunk")),
+                merged,
+            )?;
+        } else {
+            create_file_from_iter(
+                path.as_ref().join(format!("m{merged_number}.chunk")),
+                num_file_iter(&left_file),
+            )?;
+        }
+        merged_number += 1;
+    }
+    clean_up(&path)?;
+    if merged_number > 1 {
+        merge_sorted_chunks(path)?;
+    }
+    Ok(())
+}
+
+fn num_file_iter(input: &File) -> impl Iterator<Item = i64> + '_ {
+    io::BufReader::new(input)
+        .lines()
+        .filter_map(|x| x.ok())
+        .filter_map(|x| x.parse::<i64>().ok())
+}
+
+fn create_file_from_iter(
+    path: impl AsRef<Path>,
+    values: impl Iterator<Item = i64>,
+) -> io::Result<()> {
+    let mut file = File::create(path)?;
+    for n in values {
+        writeln!(file, "{n}")?;
+    }
+    Ok(())
+}
+
+fn clean_up(path: impl AsRef<Path>) -> io::Result<()> {
+    let mut chunk_number = 0_usize;
+    while fs::remove_file(path.as_ref().join(format!("{}.chunk", chunk_number))).is_ok() {
+        chunk_number += 1;
+    }
+    let mut merged_number = 0_usize;
+    while fs::rename(
+        path.as_ref().join(format!("m{}.chunk", merged_number)),
+        path.as_ref().join(format!("{}.chunk", merged_number)),
+    )
+    .is_ok()
+    {
+        merged_number += 1;
+    }
+    Ok(())
+}
+
+struct Ascending<I: Iterator<Item = i64>> {
+    left: Peekable<I>,
+    right: Peekable<I>,
+}
+
+impl<I: Iterator<Item = i64>> Ascending<I> {
+    pub fn new(left: I, right: I) -> Self {
+        Self {
+            left: left.peekable(),
+            right: right.peekable(),
+        }
+    }
+}
+
+impl<I: Iterator<Item = i64>> Iterator for Ascending<I> {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let order = match (self.left.peek(), self.right.peek()) {
+            (None, None) => None,
+            (None, Some(_)) => Some(Ordering::Greater),
+            (Some(_), None) => Some(Ordering::Less),
+            (Some(l), Some(r)) => Some(l.cmp(r)),
+        };
+        match order {
+            Some(Ordering::Equal | Ordering::Less) => self.left.next(),
+            Some(Ordering::Greater) => self.right.next(),
+            None => None,
+        }
+    }
 }
